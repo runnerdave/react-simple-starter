b38a5bd76ec14155deaafcb78153b702f7589c41
runnerdave <runnerdave@gmail.com>
2017-08-17 21:47:47 +1000

    implemented the youtube search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCWyDuXPhWl6SkE270-5Esh8d_RGczpvhQ';
+
+YTSearch({key: API_KEY, term: 'if every mountain bike video'}, function(data) {
+    console.log(data);
+})
 
 // Create a new component. This component should produce some HTML
 const App = () => {
